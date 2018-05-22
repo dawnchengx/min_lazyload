7ed7741157f7f0c4ba5cdde43a4ec614e166847f
dawnchen <dawnchen@qq.com>
2018-05-23 00:20:19 +0800

    兼容img对象数组

--- lazyload.js
+++ lazyload.js
@@ -10,10 +10,16 @@
     root.lazyload = factory();
  })(typeof global !== "undefined" ? global : this.window || this.global,  function (root) {
     function lazyload(selector){
-        var selector_all = document.querySelectorAll(selector);
-        selector_all.forEach(function(v){
-            v.src = v.getAttribute('data-src');
-        })
+        if('object' == typeof selector){
+            selector.forEach(function(v){
+                v.src = v.getAttribute('data-src');
+            })
+        }else{
+            var selector_all = document.querySelectorAll(selector);
+            selector_all.forEach(function(v){
+                v.src = v.getAttribute('data-src');
+            })
+        }
     }
     return lazyload;
  })
