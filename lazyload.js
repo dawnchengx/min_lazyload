f41360f4a07038322019f4bc90ded53dae056c59
dawnchen <dawnchen@qq.com>
2018-05-23 00:10:15 +0800

    1.0

---
+++ lazyload.js
@@ -0,0 +1,19 @@
+/*!
+ * min lazy Load - JavaScript plugin for lazy loading images
+ *
+ * Copyright (c) 2018-2018 dawnchengx
+ *
+ * Version: 1.0.0
+ * 
+ */
+ (function(root, factory){
+    root.lazyload = factory();
+ })(typeof global !== "undefined" ? global : this.window || this.global,  function (root) {
+    function lazyload(selector){
+        var selector_all = document.querySelectorAll(selector);
+        selector_all.forEach(function(v){
+            v.src = v.getAttribute('data-src');
+        })
+    }
+    return lazyload;
+ })
